83b0848ab3a4aad75a7ca0152460c13e9a9d4af2
Terence WODLING <terencewodling@gmail.com>
2018-11-24 09:26:18 +0100

    Ajout fichier Magasins

---
+++ target/classes/TestStrutsActionForm.java
@@ -0,0 +1,17 @@
+public class TestStrutsActionForm extends org.apache.struts.action.ActionForm {
+
+	private String message;
+
+	public TestStrutsActionForm() {
+		super();
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}

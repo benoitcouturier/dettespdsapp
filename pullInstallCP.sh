d3045918e1a8367a7ea2eb926738393a5d26b6d6
Terence WODLING <terencewodling@gmail.com>
2018-11-30 22:05:57 +0100

    Test .sh

--- pullInstallCP.sh
+++ pullInstallCP.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Flemme d'ecrire les commande a chaque fois donc
 git pull
-read -p "Appuyer sur une touche pour continuer ..."
+echo "Appuyer sur Entrée pour continuer..."
+read a
 mvn clean install
-read -p "Appuyer sur une touche pour continuer ..."
+echo "Appuyer sur Entrée pour continuer..."
+read a
 cp target/X-MALL-APP.war /var/lib/tomcat8/webapps/X-MALL-APP.war

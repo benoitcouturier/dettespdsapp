bc5d8654ff2db88c7ef1778966da518b5fcda6d3
Terence WODLING <terencewodling@gmail.com>
2018-11-30 22:45:12 +0100

    Liste Magasins Etape 1: vue

--- src/main/webapp/js/referentiels/magasins/listeMagasins.js
+++ src/main/webapp/js/referentiels/magasins/listeMagasins.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	$('#listeLienNav')[0].children[1].setAttribute('class','active');
 	console.log($( "#btnRecherche"));
-})
+});
 
-$( "#btnRecherche")[0].on('click',function() {
-	console.log('yes');
-  $( "#divRecherche" ).toggle( "slow" );
+$(document).ready(function(){
+	$( "#btnRecherche")[0].on('click',function() {
+		console.log('yes');
+	  $( "#divRecherche" ).toggle( "slow" );
+	});
 });

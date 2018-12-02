fae15f2216db9e00f3d76c7d7364b2cacc86cc53
Terence WODLING <terencewodling@gmail.com>
2018-12-02 12:42:52 +0100

    UC1 detail magasin

--- src/main/webapp/js/referentiels/magasins/detailMagasin.js
+++ src/main/webapp/js/referentiels/magasins/detailMagasin.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$('#listeLienNav')[0].children[1].setAttribute('class','active');
+	
+	// Type Magasin :
 	var typeMagasin = $('#idType')[0].innerHTML;
 	$.ajax({
 		headers: { 
@@ -12,11 +14,32 @@ $(document).ready(function(){
 		crossDomain : true,
 		success : function(msg) {
 			console.log(msg);
+			// lorsque que le type est récupéré on l'affiche
 			var spanType = $('#typeMagasin')[0];
 			spanType.innerHTML = msg.id +' - ' + msg.type;
 		}
 	});
-
+	
+	
+	// Emplacement :
+	var emplacement = $('#idEmplacement')[0].innerHTML;
+	$.ajax({
+		headers: { 
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		dataType : "json",
+		url : 'http://192.168.20.3:8080/ApiRest/RestGT/Emplacement/find/'+emplacement,
+		type : 'GET',
+		crossDomain : true,
+		success : function(msg) {
+			console.log(msg);
+			// lorsque que le type est récupéré on l'affiche
+			var spanEmplacement = $('#emplacement')[0];
+			spanEmplacement.innerHTML = msg.id +' - ' + msg.aile + ' - ' + msg.superficie + 'm²' + ' -  x' +msg.importance;
+		}
+	});
+	
 });
 
 
@@ -50,6 +73,7 @@ function supprimerMagasin(e){
 }
 
 function viderPage(){
+	// lors de la suppression du magasin on vide la page
 	var page = $('#page')[0];
 	page.innerHTML = ' ';
 	page.style = "margin-top : 60px;"
